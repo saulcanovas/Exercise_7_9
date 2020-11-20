fa3d5dd2677a31bd6d37cee0a884ed0121ced230
Saul <angeldenui@gmail.com>
2020-11-20 18:08:35 +0100

    Clean code
    The code was cleaned.

--- digits.cs
+++ digits.cs
@@ -1,35 +1,43 @@
+/*
+ * (c) IES San Vicente 2020
+ *
+ * Calculates how many numbers of 1, 2, 3 or more digits are
+ * entered by the user until he/she types “end”.
+ */
+
 using System;
 
-class digits
+class Digits
 {
- static void Main()
- {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++;  // it only has one digit
-       else 
-       if (input/100 == 0)
-		b ++;  // the number has 2 digits
-        else
-        if (input/1000 == 0)
-        c ++;  // the number has 3 digits
-        else
-           d++;  // the number has more than 3 digits
+    static void Main()
+    {
+        int oneDigit = 0, twoDigits = 0, threeDigits = 0, moreDigits = 0;
+
+        Console.Write("Insert a number: ");
+        string numberText = Console.ReadLine();
+
+        while (numberText != "end")
+        {
+            int number = Convert.ToInt32(numberText);
+            if (number != 0)
+            {
+                if (number / 10 == 0)
+                    oneDigit++;
+                else
+                    if (number / 100 == 0)
+                        twoDigits++;
+                    else
+                        if (number / 1000 == 0)
+                            threeDigits++;
+                        else
+                            moreDigits++;
+            }
+
+            numberText = Console.ReadLine();
         }
-      e = Console.ReadLine();
-}
 
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
+        Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", 
+            oneDigit, twoDigits, threeDigits, moreDigits);
+    }
 }
 
